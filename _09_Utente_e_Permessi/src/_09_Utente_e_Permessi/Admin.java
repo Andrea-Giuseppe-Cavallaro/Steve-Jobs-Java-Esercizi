c28a1e6197c822444a8d9c4e0d0715e1bb6d3658
Andrea-Giuseppe-Cavallaro <Adenor88@gmail.com>
2025-06-13 19:07:09 +0200

    aggiungi nuovo esercizio

--- _09_Utente_e_Permessi/src/_09_Utente_e_Permessi/Admin.java
+++ _09_Utente_e_Permessi/src/_09_Utente_e_Permessi/Admin.java
@@ -1,5 +1,19 @@
 package _09_Utente_e_Permessi;
 
-public class Admin {
+import java.util.ArrayList;
 
+public class Admin extends Utente{
+
+	public Admin(String username, String email) {
+		super(username, email);
+	}
+
+	@Override
+	public ArrayList<String> getPermessi() {
+		ArrayList<String> permessi = new ArrayList<>();
+		permessi.add("READ");
+		permessi.add("WRITE");
+		permessi.add("DELETE");
+		return permessi;
+	}
 }
--- _09_Utente_e_Permessi/src/_09_Utente_e_Permessi/GestioneUtenti.java
+++ _09_Utente_e_Permessi/src/_09_Utente_e_Permessi/GestioneUtenti.java
@@ -1,5 +1,23 @@
 package _09_Utente_e_Permessi;
 
-public class GestioneUtenti {
+import java.util.ArrayList;
 
+public class GestioneUtenti {
+	private ArrayList<Utente> utenti;
+	
+	public GestioneUtenti(){
+		utenti = new ArrayList<>();
+	}
+	
+	public GestioneUtenti aggiungiUtente(Utente u) {
+		utenti.add(u);
+		return this;
+	}
+	
+	public void stampaPermessi() {
+		for(Utente u : utenti) {
+			u.stampaInfo();
+			System.out.println("Permessi: " + u.getPermessi() + "\n");
+		}
+	}
 }
